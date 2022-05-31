d76f2a49061dacc94319d180907f1615064facf7
happinessee <gygur1206@gmail.com>
2022-05-31 20:37:26 +0900

    feat) write commands_set

---
+++ command_swap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_swap.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 20:18:19 by hyojeong          #+#    #+#             */
+/*   Updated: 2022/05/31 20:36:29 by hyojeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(t_stack *stack)
+{
+	int tmp;
+	
+	if (stack->a_size < 2)
+		return ;
+	stack->a_top
+}
+
+void	sb(t_stack stack)
+{
+
+}
+
+void	ss(t_stack stack)
+[
+	
+]
