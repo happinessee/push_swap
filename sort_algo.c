fe8f768332402568ec370f915e5451a81951fd4a
happinessee <gygur1206@gmail.com>
2022-06-06 16:06:25 +0900

    ...

--- sort_algo.c
+++ sort_algo.c
@@ -6,7 +6,7 @@
 /*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 14:30:18 by hyojeong          #+#    #+#             */
-/*   Updated: 2022/06/04 19:16:26 by hyojeong         ###   ########.fr       */
+/*   Updated: 2022/06/06 16:04:59 by hyojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,27 @@
 void	sort(t_stack *stack)
 {
 	
+}
+
+void	devide(t_stack *stack)
+{
+	const int	p1 = stack->a_size / 3;
+	const int	p2 = stack->a_size / 3 * 2;
+	int			idx;
+	t_list		*tmp;
+
+	idx = 0;
+	tmp = stack->a_top;
+	while (idx < (stack->a_size / 3))
+	{
+		if (tmp->content < p2)
+		{
+			pb(stack);
+		}
+		else if (tmp->content < p1)
+		{
+			rb(stack);
+		}
+		idx++;
+	}
 }
