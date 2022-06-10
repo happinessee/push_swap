c9010e4ee6aa7e5d4ae01ccdd71beb994a4af775
happinessee <gygur1206@gmail.com>
2022-06-10 15:25:44 +0900

    feat) add bonus

--- sort_algo.c
+++ sort_algo.c
@@ -6,13 +6,18 @@
 /*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 14:30:18 by hyojeong          #+#    #+#             */
-/*   Updated: 2022/06/04 19:16:26 by hyojeong         ###   ########.fr       */
+/*   Updated: 2022/06/04 21:56:02 by hyojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort(t_stack *stack)
+void	quick_sort(t_stack *stack)
 {
 	
+}
+
+void	greedy_sort(t_stack *stack)
+{
+
 }
